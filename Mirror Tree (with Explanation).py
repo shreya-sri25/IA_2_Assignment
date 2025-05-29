02fdaad7fbef4f9830945cab0911fdb6a7ff7b4e
shreya-sri25 <srivastavashreya624@gmail.com>
2025-05-29 08:27:19 +0530

    Update Mirror Tree (with Explanation).py

--- Mirror Tree (with Explanation).py
+++ Mirror Tree (with Explanation).py
@@ -4,28 +4,23 @@ class Node:
         self.left = None
         self.right = None
 
-# Function to convert tree to its mirror
 def mirror(root):
     if root is None:
         return None
 
-    # Step 1: Mirror left and right subtrees
     mirror(root.left)
     mirror(root.right)
 
-    # Step 2: Swap the left and right children
     root.left, root.right = root.right, root.left
 
     return root
 
-# Function for inorder traversal (for checking)
 def inorder(root):
     if root:
         inorder(root.left)
         print(root.data, end=" ")
         inorder(root.right)
 
-# Driver code
 if __name__ == "__main__":
     # Construct this binary tree
     #        1
